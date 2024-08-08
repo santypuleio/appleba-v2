220a052c4d78a70931f9974d80ffafe34c66d775
Puleio <spuleio@uade.edu.ar>
2024-08-08 20:13:48 -0300

    save arezzo files

---
+++ src/pages/data.js
@@ -0,0 +1,19 @@
+export const productos = 
+[
+    {
+        slug:"macbook-air-m2",
+        img:"https://i.pinimg.com/originals/b2/39/21/b2392123978a9779bb008b049c57fbec.png",
+        title:"MacBook Air 13 M2 chip",
+        body:"Up to 1.4x faster than MacBook Air with the M1 chip21, Support for one external display, Support for Wi-Fi 6",
+        href:"",
+        precio: "499999"
+    },
+    {
+        img:"https://www.pngall.com/wp-content/uploads/5/Apple-IPad-PNG-File-Download-Free.png",
+        title:"MacBook Air 13 M2 chip",
+        body:"Up to 1.4x faster than MacBook Air with the M1 chip21, Support for one external display, Support for Wi-Fi 6",
+        href:"",
+        slug:"ipad1",
+        precio: "299999"
+    },
+]
